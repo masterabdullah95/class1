8db3c54cc02449897cbab96ea437a36e64c76fa7
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-24 02:11:20 +0500

    Class 1 - Task 2

---
+++ task2/app.js
@@ -0,0 +1,2 @@
+var person_name = "Abdullah Shoaib";
+console.log("Hello ".concat(person_name, ", would you like to learn some Python today?"));
---
+++ task2/app.ts
@@ -0,0 +1,2 @@
+let person_name: string = "Abdullah Shoaib" 
+console.log(`Hello ${person_name}, would you like to learn some Python today?`)
