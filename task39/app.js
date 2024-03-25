7b70f27c541255d69443e68acc928c67b0ad10e5
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 04:07:17 +0500

    Class 1 - Task 39 - City Names

---
+++ task39/app.js
@@ -0,0 +1,6 @@
+var describe_city = function (city, country) {
+    console.log("\"".concat(city, ", ").concat(country, "\""));
+};
+describe_city('karachi', 'Pakistan');
+describe_city('Hyderabad', 'Pakistan');
+describe_city('Delhi', 'India');
---
+++ task39/app.ts
@@ -0,0 +1,7 @@
+let describe_city = (city, country)=>{
+    console.log(`"${city}, ${country}"`);
+}
+
+describe_city('karachi', 'Pakistan');
+describe_city('Hyderabad', 'Pakistan');
+describe_city('Delhi', 'India');
