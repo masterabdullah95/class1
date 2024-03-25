8ed150c70f5cc48a363e8dcd0c92de2430641b87
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 02:44:05 +0500

    Class 1 - Task 35 - Animals

---
+++ task35/app.js
@@ -0,0 +1,8 @@
+var animals = ['dog', 'cat', 'rabbit'];
+animals.map(function (animal) {
+    console.log(animal);
+});
+animals.map(function (animal) {
+    console.log(animal + ' is best as a pet.');
+});
+console.log('Any of these animals would make a great pet!');
---
+++ task35/app.ts
@@ -0,0 +1,11 @@
+let animals: string[] = ['dog', 'cat', 'rabbit']
+
+animals.map((animal)=>{
+    console.log(animal);
+})
+
+animals.map((animal)=>{
+    console.log(animal + ' is best as a pet.');
+})
+
+console.log('Any of these animals would make a great pet!');
