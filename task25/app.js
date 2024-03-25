dbde87168e92229f2c32cfac86cd08c8d0d0d4b8
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 01:07:35 +0500

    Class 1 - Task 25 - Alien Colors #1

---
+++ task25/app.js
@@ -0,0 +1,6 @@
+var alien_color = 'green';
+/* Write an if statement to test whether the alien’s color is green. If it is, print a message
+that the player just earned 5 points. */
+if (alien_color === 'green') {
+    console.log('player just earned 5 points.');
+}
---
+++ task25/app.ts
@@ -0,0 +1,12 @@
+let alien_color: string = 'green'
+
+/* Write an if statement to test whether the alien’s color is green. If it is, print a message 
+that the player just earned 5 points. */
+if(alien_color === 'green'){
+    console.log('player just earned 5 points.');
+}else{
+    
+}
+
+/* Write one version of this program that passes the if test and another that fails. 
+(The version that fails will have no output.) */
