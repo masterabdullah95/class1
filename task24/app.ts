be4956a79f3a4c0870b94188aa07175f2554580a
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 01:02:15 +0500

    Class 1 - Task 24 - More Conditional Tests

---
+++ task24/app.ts
@@ -0,0 +1,36 @@
+let name1: string = 'Abdullah'
+let mynum: number = 50; 
+let myarr = ['car', 'bike', 'plane']
+
+// Tests for equality and inequality with strings
+console.log(name1 === 'Abdullah');
+console.log(name1 !== 'Abdullah');
+
+console.log('-----------------');
+
+// Tests for equality and inequality with strings
+console.log(name1.toLowerCase() === 'abdullah');
+console.log(name1.toLowerCase() !== 'abdullah');
+
+console.log('-----------------');
+
+/* Numerical tests involving equality and inequality, greater than and less than, greater than 
+or equal to, and less than or equal to */
+console.log(mynum == 50);
+console.log(mynum != 50);
+console.log(mynum > 40);
+console.log(mynum < 40);
+console.log(mynum >= 50);
+console.log(mynum <= 40);
+
+console.log('-----------------');
+
+// Tests using "and" and "or" operators
+console.log((mynum == 50) && (mynum >= 50));
+console.log((mynum == 2) || (mynum < 30));
+
+console.log('-----------------');
+
+// Test whether an item is in a array
+console.log(myarr.indexOf('car') !== -1);
+console.log(myarr.indexOf('train') !== -1);
