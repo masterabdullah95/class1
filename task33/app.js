3ea9847c5020f816f22b3d23f921b508b7fa15ed
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 02:30:32 +0500

    Class 1 - Task 33 - Ordinal Numbers

---
+++ task33/app.js
@@ -0,0 +1,30 @@
+var numers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numers.map(function (val) {
+    if (val == 1) {
+        console.log('1st');
+    }
+    else if (val == 2) {
+        console.log('2nd');
+    }
+    else if (val == 3) {
+        console.log('3rd');
+    }
+    else if (val == 4) {
+        console.log('4th');
+    }
+    else if (val == 5) {
+        console.log('5th');
+    }
+    else if (val == 6) {
+        console.log('6th');
+    }
+    else if (val == 7) {
+        console.log('7th');
+    }
+    else if (val == 8) {
+        console.log('8th');
+    }
+    else if (val == 9) {
+        console.log('9th');
+    }
+});
