c69effe379a1f160ee8116d6dfb6017df1dd6ade
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-25 17:05:32 +0500

    Class 1 - Task 13 - Your Own Array

---
+++ task13/app.js
@@ -0,0 +1,5 @@
+var vehicles = ['Honda 125', 'Corolla Gli', 'Suzuki Cultus', 'Toyota Vitz'];
+console.log('I would like to own ' + vehicles[0]);
+console.log('I would like to drive ' + vehicles[1]);
+console.log('I would like to purchase ' + vehicles[2]);
+console.log('I would like to sale ' + vehicles[3]);
