28c4bb75677526efccd16ac6d3cb895145e4fa29
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 04:27:16 +0500

    Class 1 - Task 41 - Magicians

---
+++ task41/app.js
@@ -0,0 +1,7 @@
+var magicians = ['Bilal', 'Taha', 'Osama'];
+var show_magicians = function (arr) {
+    magicians.map(function (name) {
+        console.log(name);
+    });
+};
+show_magicians(magicians);
---
+++ task41/app.ts
@@ -0,0 +1,8 @@
+let magicians: string[] = ['Bilal', 'Taha', 'Osama']
+
+let show_magicians = (arr)=>{
+    magicians.map((name)=>{
+        console.log(name);
+    })
+}
+show_magicians(magicians);
