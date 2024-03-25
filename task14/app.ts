48179c7e9ee2ce7640985d28c879d9212beef20b
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-25 17:09:44 +0500

    Class 1 - Task 14 - Guest List

---
+++ task14/app.ts
@@ -0,0 +1,4 @@
+let persons: string[] = ['Kashif', 'Bilal', 'Osama']
+console.log('Hi ' + persons[0] + '! I would like to invite you for dinner.')
+console.log('Hi ' + persons[1] + '! I would like to invite you for dinner.')
+console.log('Hi ' + persons[2] + '! I would like to invite you for dinner.')
