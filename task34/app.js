b16528cb263a83201e7c6f45cbdbde012b73766e
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 02:38:33 +0500

    Class 1 - Task 34 - Pizzas

---
+++ task34/app.js
@@ -0,0 +1,8 @@
+var pizza_flavours = ['Pepperoni', 'B b q chicken', 'Pizza Margherita'];
+pizza_flavours.map(function (val) {
+    console.log(val);
+});
+pizza_flavours.map(function (val) {
+    console.log('I like the ' + val + ' pizza.');
+});
+console.log("Pizza, oh pizza, how I adore thee, With each savory bite, my taste buds set free. Cheese, sauce, and toppings in perfect harmony, Forever and always, my culinary decree. I really love pizza!");
---
+++ task34/app.ts
@@ -0,0 +1,11 @@
+let pizza_flavours: string[] = ['Pepperoni', 'B b q chicken', 'Pizza Margherita']
+
+pizza_flavours.map((val)=>{
+    console.log(val);
+})
+
+pizza_flavours.map((val)=>{
+    console.log('I like the ' + val + ' pizza.');
+})
+
+console.log("Pizza, oh pizza, how I adore thee, With each savory bite, my taste buds set free. Cheese, sauce, and toppings in perfect harmony, Forever and always, my culinary decree. I really love pizza!");
