9096ba40902ce16c22bca17d25a73fd70c7c1be8
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-25 22:24:15 +0500

    Class 1 - Task 15 - Changing Guest List

---
+++ task15/app.js
@@ -0,0 +1,6 @@
+var persons = ['Kashif', 'Bilal', 'Osama'];
+console.log(persons[0] + ' is not coming for dinner tonight.');
+persons[0] = 'Ahmed';
+console.log(persons[0] + ' is coming for dinner tonight.');
+console.log(persons[1] + ' is coming for dinner tonight.');
+console.log(persons[2] + ' is coming for dinner tonight.');
