78c3ac84eb47d05f05c451341d8100160274519e
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 01:34:59 +0500

    Class 1 - Task 29 - Favorite Fruit

---
+++ task29/app.js
@@ -0,0 +1,16 @@
+var favorite_fruits = ['apple', 'mango', 'orange'];
+if (favorite_fruits.indexOf('cherry') !== -1) {
+    console.log("You really like cherry!");
+}
+if (favorite_fruits.indexOf('watermelon') !== -1) {
+    console.log("You really like watermelon!");
+}
+if (favorite_fruits.indexOf('orange') !== -1) {
+    console.log("You really like orange!");
+}
+if (favorite_fruits.indexOf('pineapple') !== -1) {
+    console.log("You really like pineapple!");
+}
+if (favorite_fruits.indexOf('kivi') !== -1) {
+    console.log("You really like kivi!");
+}
---
+++ task29/app.ts
@@ -0,0 +1,21 @@
+let favorite_fruits: string[] = ['apple', 'mango', 'orange']
+
+if(favorite_fruits.indexOf('cherry') !== -1){
+    console.log("You really like cherry!");
+}
+
+if(favorite_fruits.indexOf('watermelon') !== -1){
+    console.log("You really like watermelon!");
+}
+
+if(favorite_fruits.indexOf('orange') !== -1){
+    console.log("You really like orange!");
+}
+
+if(favorite_fruits.indexOf('pineapple') !== -1){
+    console.log("You really like pineapple!");
+}
+
+if(favorite_fruits.indexOf('kivi') !== -1){
+    console.log("You really like kivi!");
+}
