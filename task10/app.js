84f2b40d7d145b9c986a5b11527a081b5e42e36b
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-25 16:47:48 +0500

    Class 1 - Task 10 - Adding Comments

---
+++ task10/app.js
@@ -0,0 +1,5 @@
+// In this code i have printed my name with template literals
+var person_name = "Abdullah Shoaib";
+console.log("Hello ".concat(person_name, ", would you like to learn some Python today?"));
+// In this code i have printed the quotation of my favourite person
+console.log('Ken Poirot once said, “Today is your opportunity to build the tomorrow you want.”');
