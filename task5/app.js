3c4dadf4759667d5c2757d99ac01a92297b04223
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-25 16:21:08 +0500

    Class 1 - Task 5 - Famous Quote2

---
+++ task5/app.js
@@ -0,0 +1,3 @@
+var famous_person = '“Today is your opportunity to build the tomorrow you want.”';
+var message = 'Ken Poirot once said, ' + famous_person;
+console.log(message);
---
+++ task5/app.ts
@@ -0,0 +1,3 @@
+let famous_person: string = '“Today is your opportunity to build the tomorrow you want.”'
+let message: string = 'Ken Poirot once said, ' + famous_person
+console.log(message)
