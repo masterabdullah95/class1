ce879c1d8a7a0fb1afd3ebcbc5c57127a032445e
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 01:17:49 +0500

    Class 1 - Task 27 - Alien Colors #3

---
+++ task27/app.js
@@ -0,0 +1,32 @@
+var alien_color1 = 'green';
+if (alien_color1 == "green") {
+    console.log("player earned 5 points.");
+}
+else if (alien_color1 == "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alien_color1 == "red") {
+    console.log("player earned 15 points.");
+}
+console.log('----------------');
+var alien_color2 = 'yellow';
+if (alien_color2 == "green") {
+    console.log("player earned 5 points.");
+}
+else if (alien_color2 == "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alien_color2 == "red") {
+    console.log("player earned 15 points.");
+}
+console.log('----------------');
+var alien_color3 = 'red';
+if (alien_color3 == "green") {
+    console.log("player earned 5 points.");
+}
+else if (alien_color3 == "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alien_color3 == "red") {
+    console.log("player earned 15 points.");
+}
---
+++ task27/app.ts
@@ -0,0 +1,33 @@
+let alien_color1: string = 'green'
+
+if(alien_color1 == "green"){
+    console.log("player earned 5 points.");
+}else if(alien_color1 == "yellow"){
+    console.log("player earned 10 points.");
+}else if(alien_color1 == "red"){
+    console.log("player earned 15 points.");
+}
+
+console.log('----------------');
+
+let alien_color2: string = 'yellow'
+
+if(alien_color2 == "green"){
+    console.log("player earned 5 points.");
+}else if(alien_color2 == "yellow"){
+    console.log("player earned 10 points.");
+}else if(alien_color2 == "red"){
+    console.log("player earned 15 points.");
+}
+
+console.log('----------------');
+
+let alien_color3: string = 'red'
+
+if(alien_color3 == "green"){
+    console.log("player earned 5 points.");
+}else if(alien_color3 == "yellow"){
+    console.log("player earned 10 points.");
+}else if(alien_color3 == "red"){
+    console.log("player earned 15 points.");
+}
