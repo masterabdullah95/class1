d4e962dac292f54c130a3e34d75b37c68761eb19
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-26 02:48:47 +0500

    Class 1 - Task 36 - T-Shirt

---
+++ task36/app.js
@@ -0,0 +1,4 @@
+var make_shirt = function (size, text) {
+    console.log("The size of the shirt is: ".concat(size, " and the text on the shirt is: \"").concat(text, "\""));
+};
+make_shirt(12, 'I love my DAD.');
---
+++ task36/app.ts
@@ -0,0 +1,5 @@
+const make_shirt = (size: number, text: string) =>{
+    console.log(`The size of the shirt is: ${size} and the text on the shirt is: "${text}"`);
+}
+
+make_shirt(12, 'I love my DAD.');
