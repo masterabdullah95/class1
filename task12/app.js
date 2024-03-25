d53c06e0ad4252f10f4f1a550613670c37c6df36
Abdullah Shoaib <masterabdullah95@gmail.com>
2024-03-25 16:54:52 +0500

    Class 1 - Task 12 - Greetings

---
+++ task12/app.js
@@ -0,0 +1,5 @@
+var names = ['Osama', 'Bilal', 'Taha', 'Qasim'];
+console.log('How are you ' + names[0] + '?');
+console.log('How are you ' + names[1] + '?');
+console.log('How are you ' + names[2] + '?');
+console.log('How are you ' + names[3] + '?');
